d4dc39b1264af506ce15678fad7ddf4ce451a8d4
santhoshkannan007 <myecommercesyt@gmail.com>
2026-08-02 08:52:48 +0530

    Remove IQAC role option and redirect from login screens

--- app/iqac/remarks/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { IqacWorkspace } from '@/components/roles/IqacWorkspace';
-
-export default function IqacRemarksPage() {
-  return <IqacWorkspace view="remarks" />;
-}
